ee2622755d012a7abbb1bb148728ffbb5ee9bf2d
imene prince <marvinprince232@gmail.com>
2025-05-24 14:42:08 +0300

    clean

--- yt_notes/settings.py
+++ yt_notes/settings.py
@@ -130,15 +130,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
 STATICFLIES_DIRS=os.path.join(BASE_DIR,'static')
 STATIC_ROOT = os.path.join(BASE_DIR,'staticflies_build','static')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
