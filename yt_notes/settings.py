62e96cef038c64d010b6f5f10c05ca91a2682256
imene prince <marvinprince232@gmail.com>
2025-05-24 12:58:06 +0300

    changes

--- yt_notes/settings.py
+++ yt_notes/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1u66v$+2_z4p^=u5+o8^n510ogx(11do2(!b_o^b-pcne!yxxm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://django-note-app-naxf.onrender.com","django-note-app-naxf.onrender.com"]
 
 
 # Application definition
